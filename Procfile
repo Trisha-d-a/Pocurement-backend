d240db2d9749346b56b7e8708cdbd60e5d33f0b8
trishaanandan <trisha.anandan@gmail.com>
2025-09-26 12:43:12 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn app.mainapi:app --host 0.0.0.0 --port $PORT
---
+++ utils/qr_generator.py
@@ -0,0 +1,5 @@
+I, Trisha D.A. of 2nd year CSE(AI) am going abroad (DUBAI) for an Educational Trip organized by our College for  "INTERNATIONAL CREDIT COURSE" and "INTERNATIONAL CONFERENCE". I'll also be doing a "PAPER PRESENTATION".
+
+So, I kindly request you to grant me OD for these days.
+
+Thank you.
